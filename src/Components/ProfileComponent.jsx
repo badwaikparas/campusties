c285e195af29ab680bced8cadc2c17a955c464de
badwaikparas <badwaikparas@gmail.com>
2025-04-23 17:00:26 +0530

    finishing breaks at 1083

--- src/Components/ProfileComponent.jsx
+++ src/Components/ProfileComponent.jsx
@@ -14,44 +14,46 @@ export default function ProfileComponent({ currentUser }) {
     console.log(JSON.stringify(currentUser));
 
     return (
-        <div className='flex justify-center items-start overflow-y-auto gap-8 p-4'>
-            {/* Left */}
-            <div className='w-full'>
-                <Card paddingX={0} paddingY={0}>
-                    <ProfileCard currentUser={currentUser} />
-                </Card>
-                <Card paddingX={30} paddingY={30}>
-                    <ProfileAboutCard />
-                </Card>
+        <div className='flex justify-center'>
+            <div className='flex justify-center items-start overflow-y-auto gap-8 p-4 w-3/4'>
+                {/* Left */}
+                <div className='w-full'>
+                    <Card paddingX={0} paddingY={0}>
+                        <ProfileCard currentUser={currentUser} />
+                    </Card>
+                    <Card paddingX={30} paddingY={30}>
+                        <ProfileAboutCard />
+                    </Card>
 
 
-                {/* Projects Section */}
-                <Card paddingX={30} paddingY={30}>
-                    <ProfileProject />
-                </Card>
+                    {/* Projects Section */}
+                    <Card paddingX={30} paddingY={30}>
+                        <ProfileProject />
+                    </Card>
 
-                <Card paddingX={30} paddingY={30}>
-                    <ProfileSkills />
-                </Card>
+                    <Card paddingX={30} paddingY={30}>
+                        <ProfileSkills />
+                    </Card>
 
-                <Card paddingX={30} paddingY={30}>
-                    <ProfileExperience />
-                </Card>
-                <Card paddingX={30} paddingY={30}>
-                    <ProfileEducation />
-                </Card>
-            </div>
-            {/* Right */}
-            <div className='w-[400px]  '>
-                <Card paddingX={30} paddingY={30}>
-                    <ProfileDashboard />
-                </Card>
-                <Card paddingX={30} paddingY={30}>
-                    <ProfileVisitors />
-                </Card>
-                <Card paddingX={30} paddingY={30}>
-                    <ProfileBlogs />
-                </Card>
+                    <Card paddingX={30} paddingY={30}>
+                        <ProfileExperience />
+                    </Card>
+                    <Card paddingX={30} paddingY={30}>
+                        <ProfileEducation />
+                    </Card>
+                </div>
+                {/* Right */}
+                <div className='w-[400px]  '>
+                    <Card paddingX={30} paddingY={30}>
+                        <ProfileDashboard />
+                    </Card>
+                    <Card paddingX={30} paddingY={30}>
+                        <ProfileVisitors />
+                    </Card>
+                    <Card paddingX={30} paddingY={30}>
+                        <ProfileBlogs />
+                    </Card>
+                </div>
             </div>
         </div>
     )
